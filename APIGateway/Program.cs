f55044f223e90d8f4008916c0162b86f6aa84ba5
KamranFrazex <kismayilov@frazex.com>
2024-05-24 14:25:24 +0400

    Add project files.

---
+++ APIGateway/Program.cs
@@ -0,0 +1,40 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddOcelot();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.UseOcelot();
+
+            app.Run();
+        }
+    }
+}
